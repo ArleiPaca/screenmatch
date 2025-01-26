ea5d3309760485cfdd11d2bf0ae7709ea31f4b96
ArleiPaca <aplepiani@gmail.com>
2025-01-26 12:43:12 -0300

    first commit

---
+++ src/main/java/br/com/arlei/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,25 @@
+package br.com.arlei.screenmatch;
+
+import br.com.arlei.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+    	System.out.println(json);
+    	json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+	}
+}
+
