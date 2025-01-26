1c93a8cc6e5efbef5702cc78eecf226089a2b73d
ArleiPaca <aplepiani@gmail.com>
2025-01-26 13:49:39 -0300

    segundo commit

--- src/main/java/br/com/arlei/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/arlei/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.arlei.screenmatch;
 
+import br.com.arlei.screenmatch.model.DadosSerie;
 import br.com.arlei.screenmatch.service.ConsumoAPI;
+import br.com.arlei.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,8 +20,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConsumoAPI consumoApi = new ConsumoAPI();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
     	System.out.println(json);
-    	json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
+    	//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		//System.out.println(json);
+		//System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+
 	}
 }
 
---
+++ src/main/java/br/com/arlei/screenmatch/model/DadosSerie.java
@@ -0,0 +1,13 @@
+package br.com.arlei.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+// aqui iremos desserializar o JSON que a API nos retorna o jsoproperty tbm serve para renomear o atributo, porepm no alis podemusar array
+// para renomear mais de um atributo
+// Anotação para ignorar propriedades desconhecidas
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+}
